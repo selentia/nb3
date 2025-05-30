03e97079f5b4339ba2d66f2c4b4c48acbaad8708
selentia <admin@duna.me>
2025-05-30 12:17:31 +0900

    validateProduct .every 통합

--- mission1/src/utils/validateProduct.js
+++ mission1/src/utils/validateProduct.js
@@ -4,7 +4,7 @@ const validateProduct = (data) => {
   if (typeof data.description !== 'string') return false;
   if (typeof data.price !== 'number' || data.price < 0) return false;
   if (!Array.isArray(data.tags) || !data.tags.every(i => typeof i === 'string')) return false;
-  if (!Array.isArray(data.images) || !data.images.every(i => typeof i === 'string') || !data.images.every(url => url.startsWith('http'))) return false;
+  if (!Array.isArray(data.images) || !data.images.every(url => typeof url === 'string' && url.startsWith('http'))) return false;
   if (typeof data.createdAt !== 'string') return false;
   return true;
 }
